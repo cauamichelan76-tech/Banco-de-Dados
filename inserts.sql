fa04120d7fd087054879d202448e68b0a5eb6a8d
Cauã Michelan <cauamichelan76@gmail.com>
2025-11-29 22:22:30 -0300

    Create  inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,4 @@
+INSERT INTO Cliente (nome, cpf, telefone, data_nascimento) VALUES
+('Ana Souza', '12345678901', '11987654321', '1995-04-10'),
+('Carlos Lima', '98765432100', '11999887766', '1990-08-22'),
+('Mariana Reis', '45678912300', '11977776666', '2001-11-05');
